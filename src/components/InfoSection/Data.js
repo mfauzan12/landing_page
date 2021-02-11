3c17100d68a77023ec42210eeef74d040df87cdd
fauzan muhammad <mfauzan321@yahoo.co.id>
2021-02-11 07:19:24 +0700

    commit pertama

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,50 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Premium Bank',
+    headline: 'Unlimited Transaction with zero fees',
+    description: "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.",
+    buttonLabel: 'Get Started',
+    imgStart: false,
+    img: require('../../images/svg-1.svg').default,
+    alt: 'test',
+    dark: true,
+    primary: true,
+    darkText: false
+}
+
+export const homeObjTwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Unlimited Access',
+    headline: 'Unlimited Transaction with zero fees',
+    description: "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.",
+    buttonLabel: 'Learn More',
+    imgStart: true,
+    img: require('../../images/svg-2.svg').default,
+    alt: 'test2',
+    dark: false,
+    primary: false,
+    darkText: true
+}
+
+export const homeObjThree = {
+    id: 'signup',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Join our team',
+    headline: 'creating an account is extremely easy',
+    description: "get everything set up and ready in under 10 minutes. all you need you need to do is add your information and you're ready to go",
+    buttonLabel: 'Start now',
+    imgStart: false,
+    img: require('../../images/svg-1.svg').default,
+    alt: 'test3',
+    dark: false,
+    primary: false,
+    darkText: true
+}
